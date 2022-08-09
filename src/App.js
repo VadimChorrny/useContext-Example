88f68c3f77ab3f72ab8b9c2f92fb4a04509c6ea6
VadymChorny <chorrny228@gmail.com>
2022-08-09 14:32:36 +0300

    first coomit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { Context } from './context/Context';
+import CarDetails from './pages/CarPage/CarDetails';
+import './styles/Index.css';
 
 function App() {
+  const data = {
+    name: 'Vadym',
+    logo: 'https://yt3.ggpht.com/ytc/AMLnZu8W4zHc3gYJmK1Z8U1HCuZOdRpr1YaGhOEpr9ek3w=s900-c-k-c0x00ffffff-no-rj',
+    themes: {
+      dark: {
+        background: '#17202A',
+      },
+      light: {
+        background: '#fff',
+      },
+      language: {
+        ua: {
+          logo: 'Добрий день',
+          main_text:
+            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        },
+        en: {
+          logo: 'Hello everybody!',
+          main_text: '',
+        },
+      },
+    },
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Context.Provider value={data}>
+        {' '}
+        {/* Second Step - Initialization */}
+        <CarDetails />
+      </Context.Provider>
     </div>
   );
 }
---
+++ src/context/Context.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+export const Context = createContext('Deafult value');
+
+// FIST STEP - CREATING CONTEXT
